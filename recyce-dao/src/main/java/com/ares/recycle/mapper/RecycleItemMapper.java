27aac54c8f72bb804ca8270a1344a7260aca6766
WangQiaomu <wangqiaomu@aikucun.com>
2019-07-09 16:55:20 +0800

    1. modify

--- recyce-dao/src/main/java/com/ares/recycle/mapper/RecycleItemMapper.java
+++ recyce-dao/src/main/java/com/ares/recycle/mapper/RecycleItemMapper.java
@@ -2,10 +2,12 @@ package com.ares.recycle.mapper;
 
 import com.ares.recycle.po.RecycleItemPO;
 import com.ares.recycle.request.RecycleItemRequest;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
+@Mapper
 public interface RecycleItemMapper {
 
     int insert(RecycleItemPO record);
