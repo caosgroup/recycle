159394273aae61aad7fb1bec6ce56c9d24ccd7a8
WangQiaomu <wangqiaomu@aikucun.com>
2019-07-09 17:02:01 +0800

    1. support fuzzy query

--- recyce-service/src/main/java/com/ares/recycle/service/impl/RecycleServiceImpl.java
+++ recyce-service/src/main/java/com/ares/recycle/service/impl/RecycleServiceImpl.java
@@ -8,6 +8,7 @@ import com.ares.recycle.request.RecycleItemRequest;
 import com.ares.recycle.service.RecycleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
 
@@ -23,6 +24,9 @@ public class RecycleServiceImpl implements RecycleService {
 
     @Override
     public List<RecycleItemBO> list(RecycleItemRequest request) {
+        if (!StringUtils.isEmpty(request.getName())) {
+            request.setName("%" + request.getName() + "%");
+        }
         List<RecycleItemPO> poList = recycleItemMapper.list(request);
         return BeanCopyUtils.copy(poList, RecycleItemBO.class);
     }
